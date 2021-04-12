a4e6db8bfc4db054d5ac26c512ef942cf3593f3a
Vasilii Karpenko <invweb@gmail.com>
2021-04-12 17:32:46 +0300

    added viewBiding and dataBinding

--- app/src/main/java/com/spacex/adapters/MissionAdapter.kt
+++ app/src/main/java/com/spacex/adapters/MissionAdapter.kt
@@ -3,55 +3,59 @@ package com.spacex.adapters
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.databinding.ViewDataBinding
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.spacex.database.entity.Launch
+import com.test.spacex.BR
 import com.test.spacex.R
+import com.test.spacex.databinding.LaunchesBinding
 
 class MissionAdapter(val context: Context) : RecyclerView.Adapter<MissionAdapter.MyViewHolder>() {
     private var localLaunches: List<Launch> = emptyList()
 
-    fun setLaunches(setedLaunches: List<Launch>){
-        localLaunches = setedLaunches
+    fun setLaunches(launches: List<Launch>){
+        localLaunches = launches
         notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val launch : Launch = localLaunches[position]
+
         launch.let { holder.setLaunchItem(it) }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view : View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.launches, parent, false)
-        return MyViewHolder(view, context)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: LaunchesBinding = inflate(inflater, R.layout.launches, parent,
+            false)
+        return MyViewHolder(binding, context)
     }
 
     override fun getItemCount(): Int = localLaunches.size
 
-    class MyViewHolder(itemView: View, val context: Context) : RecyclerView.ViewHolder(itemView) {
-        private val textMissionFlightNameView : TextView = itemView.findViewById(R.id.mission_flight_name)
-        private val textMissionFlightNumber : TextView = itemView.findViewById(R.id.mission_flight_number)
-        private val textLaunchYear : TextView = itemView.findViewById(R.id.mission_launch_year)
-        private val image : ImageView = itemView.findViewById(R.id.image)
+    class MyViewHolder(launchesBinding: LaunchesBinding, private val context: Context)
+        : RecyclerView.ViewHolder(launchesBinding.root)  {
+
+        private val binding: ViewDataBinding = launchesBinding
 
         fun setLaunchItem(launchItem: Launch){
-            textMissionFlightNameView.text = launchItem.missionName
-            textMissionFlightNumber.text = launchItem.flightNumber.toString()
-            textLaunchYear.text = launchItem.launchYear.toString()
+            binding.setVariable(BR.launch, launchItem)
 
-            itemView.setOnClickListener {
+            binding.root.setOnClickListener {
                 val itemBundle = Bundle()
                 itemBundle.putParcelable("launch", launchItem)
                 it.findNavController().navigate(R.id.startFragment, itemBundle)
             }
 
-            Glide.with(context).load(launchItem.links.missionPatchSmall).into(image)
+            Glide.with(context).load(launchItem.links.missionPatchSmall).into(
+                binding.root.findViewById(R.id.image)
+            )
         }
     }
 }
