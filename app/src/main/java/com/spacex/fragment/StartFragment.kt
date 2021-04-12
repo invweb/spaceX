0df20f8308952d30d3f309b6f0d2199291fe3a51
Vasilii Karpenko <invweb@gmail.com>
2021-04-12 15:26:21 +0300

    added viewBiding and dataBinding

--- app/src/main/java/com/spacex/fragment/StartFragment.kt
+++ app/src/main/java/com/spacex/fragment/StartFragment.kt
@@ -9,7 +9,6 @@ import com.bumptech.glide.Glide
 import com.spacex.database.entity.Launch
 
 import com.test.spacex.R
-import com.test.spacex.databinding.FragmentErrorBinding
 import com.test.spacex.databinding.FragmentStartBinding
 import kotlinx.android.synthetic.main.fragment_start.*
 
@@ -28,23 +27,12 @@ class StartFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val launch: Launch? = requireArguments().getParcelable<Launch>("launch")
+        startFragmentBinding.launch = launch
         if (launch?.details.equals("empty")) {
             startFragmentBinding.detailsText.text = getString(R.string.details_is_empty)
         } else {
             startFragmentBinding.detailsText.text = launch?.details
         }
-
-        startFragmentBinding.rocketName.text = launch?.rocket?.rocketName
-        startFragmentBinding.rocketType.text = launch?.rocket?.rocketName
-
-        startFragmentBinding.coreText.text = launch?.reuse?.core.toString()
-        startFragmentBinding.sideCore1Text.text = launch?.reuse?.side_core1.toString()
-        startFragmentBinding.sideCore2Text.text = launch?.reuse?.side_core2.toString()
-        startFragmentBinding.fairingsText.text = launch?.reuse?.fairings.toString()
-        startFragmentBinding.capsuleText.text = launch?.reuse?.capsule.toString()
-
-        startFragmentBinding.missionFlightNumberText.text = launch?.flightNumber.toString()
-
         Glide.with(this).load(launch?.links?.missionPatchSmall).into(mission_image)
     }
 }
